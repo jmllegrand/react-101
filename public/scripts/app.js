b2c11dc5ecdf84a91c9088f353032feba07e3a0f
jmllegrand <jml.legrand@gmail.com>
2016-03-17 22:55:28 +0100

    Ganerate a list dynamically from a local JSON

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,6 +2,12 @@
  * Created by jmlegrand on 23/02/16.
  */
 
+var datas = [
+  {id: 1, author: "Pete Hunt", text: "This is one comment"},
+  {id: 2, author: "Jordan Walke", text: "This is *another* comment"}
+];
+
+
 
 var CommentBox = React.createClass({
   displayName: 'CommentBox',
@@ -9,7 +15,7 @@ var CommentBox = React.createClass({
     return (
       <div className="commentBox">
         <h1>comments</h1>
-        <CommentList />
+        <CommentList data={this.props.data}/>
         <CommentForm />
       </div>
     );
@@ -20,10 +26,16 @@ var CommentBox = React.createClass({
 var CommentList = React.createClass({
   displayName: 'CommentList',
   render: function () {
+    var commentNodes = this.props.data.map(function(comment) {
+      return (
+        <Comment author={comment.author} key={comment.id}>
+          {comment.text}
+        </Comment>
+      );
+    });
     return (
-      <div>
-        <Comment author="Pete Hunt">This is one comment</Comment>
-        <Comment author="Jordan Walke">This is *another* comment</Comment>
+      <div className="commentList">
+        {commentNodes}
       </div>
     );
   }
@@ -56,7 +68,7 @@ var CommentForm = React.createClass({
 
 
 ReactDOM.render(
-  <CommentBox/>,
+  <CommentBox data={datas}/>,
   document.getElementById('content')
 );
 
